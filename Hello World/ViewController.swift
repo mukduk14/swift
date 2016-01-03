10a12442fc92b844ea27405e07512f2186da97cd
Shan Singh Tinna <punjabishan@gmail.com>
2016-01-02 20:44:52 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Shan Singh Tinna on 1/2/16.
+//  Copyright © 2016 Shan Singh Tinna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
